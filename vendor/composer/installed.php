c0ef3650fdad253d3aae1add98e5cd4ccf09edea
Francis <fmdp93@gmail.com>
2024-03-30 19:55:00 +0800

    deleted monolog

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fmdp93/hello-world',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '858c38ddae2bc376f1dd46882ce35a604718e0d5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fmdp93/hello-world' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '858c38ddae2bc376f1dd46882ce35a604718e0d5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
